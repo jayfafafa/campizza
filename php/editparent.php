5e215330bb291d46597b8a205818995b1cf93729
Noah <nkkenji123@gmail.com>
2019-02-06 20:05:31 -0800

    add back to dashboard button to editparent

--- php/editparent.php
+++ php/editparent.php
@@ -137,6 +137,10 @@ unset($conn);
 <div class="container" style = "background: white; margin-top: 20px;">
 
     <h1 style = "padding-top: 40px;text-align: center;">Registration for Camp Izza</h1>
+    <div class="ml-auto">
+		<a class="btn btn-primary top-buffer" href="dashboard.php" role="button">< Back to Dashboard</a>
+							
+	</div>
 
   	<div class="container">
 
